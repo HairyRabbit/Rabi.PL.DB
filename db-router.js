f19be9f621c84ca5e51ccc37ba8f15a77e339411
0x00-pl <0x00.pl@gmail.com>
2017-05-07 03:39:19 +0800

    add more tests

--- db-router.js
+++ db-router.js
@@ -7,6 +7,13 @@ let impl = require('./db-router-impl')
 let key = 'rabbit and pl hold the key'
 let global_connection_dict = {}
 
+router.get('/hello', function (req, res) {
+    res.send('world')
+})
+
+router.get('/helloj', function (req, res) {
+    res.json({j: 'j'})
+})
 
 router.use(express_jwt({secret: key}).unless({path: ['/connect']}))
 
@@ -26,6 +33,16 @@ router.get('/connect', function (req, res) {
         res.json({err})
     })
 })
+
+router.get('/end', (req, res) => {
+    let connection_token = req.user.token
+    let connection = global_connection_dict[connection_token]
+    connection.end()
+    delete global_connection_dict[connection_token]
+    res.end()
+})
+
+
 
 module.exports = router
 
--- db-router.test.js
+++ db-router.test.js
@@ -1,4 +1,5 @@
 let impl = require('./db-router-impl')
+let dbr = require('./db-router')
 
 
 test('hello Promise', done => {
@@ -30,3 +31,45 @@ test('connect mysql badpass', done => {
         done()
     })
 })
+
+test('dbr router has handle', () => {
+    expect(dbr.handle).toBeDefined()
+})
+
+test('dbr router hello', () => {
+    dbr.handle({url: '/hello', method: 'GET'}, {send: function (v) {
+        expect(v).toEqual('world')
+    }})
+})
+
+test('test helloj', () => {
+    dbr.handle({url: '/helloj', method: 'GET'}, {json: function (v) {
+        expect(v.j).toBeDefined()
+    }})
+})
+
+
+let auth_headers
+
+
+beforeAll(() => {
+    let query = {
+        host: '127.0.0.1', port: 3306,
+        user: 'root', pass: '',
+        database: 'mysql',
+        ssl: 'disabled'
+    }
+
+    dbr.handle({url: '/connect', query, method: 'GET'}, {json (v) {
+        auth_headers = {authorization: 'Bearer ' + v.token}
+        expect(v.token).toBeDefined()
+    }})
+})
+
+afterAll(() => {
+    dbr.handle({url: '/end', method: 'GET', headers: auth_headers}, {end () {
+        expect(1).toBeDefined()
+    }})
+})
+
+
