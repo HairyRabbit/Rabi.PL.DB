25378a9601d9fa3f23996f762c15bde416f7973a
yuffiy <yfhj1990@hotmail.com>
2017-07-01 12:58:20 +0800

    Add remove jsonp appended script tag and fix type error.

--- frontend/src/component/amap/lib/load-map.js
+++ frontend/src/component/amap/lib/load-map.js
@@ -7,43 +7,74 @@
  *
  * Load AMap instance by jsonp.
  *
- * @require {string} AMAP_URL 
- * @require {string} AMAP_KEY
+ * TODO support amap plugins preload via AMAP_PLUGINS.
+ *
+ * @env {string} AMAP_URL
+ * @env {string} AMAP_KEY
+ * @env {string} AMAP_VERSION
  */
 
+import invariant from 'invariant'
 import getRoot from 'lib/get-root'
 import registerJsonp from 'lib/register-global-jsonp'
 
-function loadMap(key: string, plugins: Array<string>): Promise<*> {
-  const root = getRoot()
+type Options = {
+  url?: string,
+  version?: string,
+  key?: string,
+  plugins?: Array<string>
+}
+
+const defaultOptions = {
+  plugins: []
+}
+
+function jsonp(
+  resolve: Function,
+  reject: Function,
+  global: Object,
+  callback: Function
+): Function {
+  return function(): void {
+    global.AMap ? resolve(global.AMap) : reject()
+    callback()
+  }
+}
+
+function makeRequestUrl(options: Options): Function {
+  const url: ?$PropertyType<Options, 'url'> =
+    options.url || process.env.AMAP_URL
+  const version: ?$PropertyType<Options, 'version'> =
+    options.version || process.env.AMAP_VERSION
+  const key: ?$PropertyType<Options, 'key'> =
+    options.key || process.env.AMAP_KEY
+  const plugins: $PropertyType<Options, 'plugins'> =
+    options.plugins || defaultOptions.plugins
+  const error_message: string = 'Must provide AMap JSSDK %s'
+
+  invariant(url, error_message, 'url')
+  invariant(version, error_message, 'version')
+  invariant(key, error_message, 'key')
+
+  return function(callback: string): string {
+    // TODO ${key ? `key=${key}&` : ''}\
+    return `\
+${url}?\
+v=${version}&\
+key=${key}&\
+${plugins && plugins.length !== 0 ? `plugin=${plugins.join(',')}&` : ''}\
+callback=${callback}`
+  }
+}
+
+function loadMap(options?: Options): Promise<*> {
+  const root: Object = getRoot()
 
   if (root.AMap) {
     return Promise.resolve(root.AMap)
   }
 
-  function jsonp(resolve, reject, global, callback): Function {
-    return function(): void {
-      global.AMap ? resolve(global.AMap) : reject()
-      console.log(callback)
-      callback()
-    }
-  }
-
-  function makeAMapUrl(key: string, plugins: Array<string>): Function {
-    return function(callback): string {
-      if (!process.env.AMAP_URL) {
-        throw new Error(`Not Found AMAP_URL defined.`)
-      }
-
-      return `\
-${process.env.AMAP_URL}&\
-${key ? `key=${key}&` : ''}\
-${plugins.length !== 0 ? `plugin=${plugins.join(',')}&` : ''}\
-callback=${callback}`
-    }
-  }
-
-  return registerJsonp(jsonp, makeAMapUrl(key || null, plugins || []))
+  return registerJsonp(jsonp, makeRequestUrl(options || {}))
 }
 
 export default loadMap
--- frontend/src/lib/register-global-jsonp.js
+++ frontend/src/lib/register-global-jsonp.js
@@ -7,26 +7,55 @@
  *
  * Register a jsonp on global object with a promise.
  *
- * @require {Object} window
+ * @env {string} JSONP_PREFIX
+ * @env {string} JSONP_SIZE
  */
 
 import getRoot from 'lib/get-root'
 import makeJsonpScript from 'lib/make-jsonp-script'
 import randomString from 'lib/random-string'
 
-function registerGlobalJsonp(jsonp: Function, callback: Function): Promise<*> {
-  const global: Object = getRoot()
-  const key: string = `jsonp_${randomString(12)}`
-  const script: HTMLElement = makeJsonpScript(callback(key))
+type Options = {
+  prefix?: string,
+  size?: number
+}
 
-  function clearGlobalJsonpCallback(): void {
+const defaultOptions: Options = {
+  prefix: 'jsonp',
+  size: 12
+}
+
+function registerGlobalJsonp(
+  jsonp: Function,
+  feedback: Function,
+  options?: Options
+): Promise<*> {
+  const global: Object = getRoot()
+  const opt: Options = { ...options }
+  const prefix: $PropertyType<Options, 'prefix'> =
+    opt.prefix || process.env.JSONP_PREFIX || defaultOptions.prefix
+  const size: $PropertyType<Options, 'size'> =
+    opt.size || process.env.JSONP_SIZE || defaultOptions.size
+  const key: string = `${prefix}_${randomString(size)}`
+  const script: HTMLElement = makeJsonpScript(feedback(key))
+
+  function removeScriptTag(): void {
+    // FIXME Care about `document`
+    document.head.removeChild(script)
+  }
+
+  function destroyJsonpCallback(callback: Function): void {
     setTimeout(function() {
+      global[key] = null
       delete global[key]
+
+      // NOTE Used remove script tag
+      callback && callback(removeScriptTag)
     })
   }
 
   return new Promise(function(resolve: Function, reject: Function): void {
-    global[key] = jsonp(resolve, reject, global, clearGlobalJsonpCallback)
+    global[key] = jsonp(resolve, reject, global, destroyJsonpCallback)
     script.onerror = reject
     document.head.appendChild(script)
   })
