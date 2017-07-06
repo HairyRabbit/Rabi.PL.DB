6f0fe063e783d8a132d913ced0247d651b2a8fda
yuffiy <yfhj1990@hotmail.com>
2017-07-06 17:38:33 +0800

    Add postcss-calc plugin.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     //require('autoprefixer')(),
     //require('postcss-custom-properties')(),
+    require('postcss-calc'),
     require('postcss-short')(),
     require('postcss-nesting')(),
     require('postcss-color-function')(),
